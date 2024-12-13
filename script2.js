c6d8a7940493f9669c809a9a48b3a04394907d52
Oleg171984 <kucugurnyj171984@gmail.com>
2024-12-13 17:03:08 +0200

    Removed unnecessary files and kept only task1, task2, task3

--- script2.js
+++
@@ -1,7 +0,0 @@
-
-const imageCount = 9;
-const randomIndex = Math.floor(Math.random() * imageCount) + 1;
-const imagePath = `images/${randomIndex}.jpg`;
-const imageElement = document.getElementById('random-image');
-imageElement.src = imagePath;
-imageElement.alt = `Random Image ${randomIndex}`;
--- script3.js
+++
@@ -1,8 +0,0 @@
-
-    const textBlock = document.getElementById('text-block');
-    const button = document.getElementById('toggle-color-button');
-
-    button.addEventListener('click', () => {
-    textBlock.style.color = textBlock.style.color === 'black' ? 'red' : 'black';
-});
-
